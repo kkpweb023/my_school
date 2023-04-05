7e392033affab96cbaa625ba8e97e54329298de7
kkpweb023 <kpandey023@gmail.com>
2023-04-05 18:36:26 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import NavBar from './Services/NavBar';
+import Footer from './Services/Footer';
 
 function App() {
   return (
-    <div className="App">
- 
-    </div>
+    <div style={{ position: "relative" }}>
+    <NavBar />
+    <Routes basename="/school_notes">
+        
+        <Route>
+            <Route path='/' element={<h1>Home</h1>} />
+            <Route path='/notes/:info' element={<h1>Class 9 page</h1>} />         
+        </Route>
+
+    </Routes>
+    <Footer />
+
+</div>
   );
 }
 
---
+++ src/Services/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.css';
+
+
+const NavBar = () => {
+
+  return (
+
+    <div>
+          <ul className='nav-link-ul'>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/notes/class_9'>Class 9</Link></li>
+          </ul>
+    </div>
+  )
+}
+export default NavBar;
