bf18fdcbd55139746c97f6047c71d22a7348b689
kkpweb023 <kpandey023@gmail.com>
2023-04-22 22:08:19 +0530

    modify side menu bar

--- src/Service/NavBar/AppBar.js
+++ src/Service/NavBar/AppBar.js
@@ -1,172 +1,279 @@
-import React, { useContext } from 'react';
-import Container from 'react-bootstrap/Container';
-import {Nav, Navbar, NavDropdown } from 'react-bootstrap';
-import { Link, NavLink } from 'react-router-dom';
-import Offcanvas from 'react-bootstrap/Offcanvas';
-import { MyContext } from '../../App.js';
-import { useNavigate } from 'react-router-dom';
-
-
+import React, { useContext } from "react";
+import Container from "react-bootstrap/Container";
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Link, NavLink } from "react-router-dom";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import { MyContext } from "../../App.js";
+import { useNavigate } from "react-router-dom";
+import "./AppBar.css";
 
 function AppBar() {
+  const { showMob, setShowMob } = useContext(MyContext);
 
-    const {
-        showMob, 
-        setShowMob } = useContext(MyContext);
+  const navigate = useNavigate();
+  const auth = localStorage.getItem("user");
 
-        const navigate = useNavigate();
-        const auth = localStorage.getItem('user');
-      
-        function UserLogout() {
-          localStorage.clear();
-          setShowMob(false)
-          navigate('/signUp');
-        }
+  function UserLogout() {
+    localStorage.clear();
+    setShowMob(false);
+    navigate("/signUp");
+  }
 
-        function handleExam(){
+  function handleExam() {
+    if (!auth) {
+      alert("Only for admin or login first....");
+    }
+    setShowMob(false);
+  }
 
-            if(!auth){
-                alert("Please login first....")
-            }
-            setShowMob(false)
-        }
-      
-      
-      
+  return (
+    <>
+      <Navbar
+        variant="light"
+        expand={"sm"}
+        style={{ fontSize: "14px", backgroundColor: "rgb(212, 223, 65)" }}
+      >
+        <Container fluid>
+          <Navbar.Toggle
+            aria-controls={`offcanvasNavbar-expand-sm`}
+            bg="dark"
+            variant="dark"
+            onClick={() => setShowMob(true)}
+          />
+
+          <Navbar.Offcanvas
+            id={`offcanvasNavbar-expand-sm`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-sm`}
+            style={{ backgroundColor: "rgb(212, 223, 65)", color: "white" }}
+            show={showMob}
+            onHide={() => setShowMob(false)}
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title
+                id={`offcanvasNavbarLabel-expand-sm`}
+                className="me-5 ms-3 fs-1 fw-bold"
+                style={{ color: "green" }}
+              >
+                My School
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+
+            <hr style={{ height: "2px", color: "blue" }} />
+
+            <Offcanvas.Body
+              className="off_body"
+              onTouchMove={() => setShowMob(false)}
+            >
+
+            { /*====================== Home ======================== */   }
+
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Nav.Link
+                  as={NavLink}
+                  to="/"
+                  className="app_menu"
+                  onClick={() => setShowMob(false)}
+                >
+                  <i className="bi bi-house-door-fill"></i>
+                  <span> Home</span>
+                </Nav.Link>
 
 
-    return (
-        <>
-            <Navbar variant="light" expand={'sm'}  style={{ fontSize: "14px",backgroundColor:"rgb(212, 223, 65)"}} >
-                
-                <Container fluid>
-                     
-                    <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-sm`} bg="dark" variant="dark" onClick={() => setShowMob(true)} />
+                { /*====================== Course ======================== */   }
 
 
-                    <Navbar.Offcanvas
-                        id={`offcanvasNavbar-expand-sm`}
-                        aria-labelledby={`offcanvasNavbarLabel-expand-sm`}
-                        style={{ backgroundColor: "rgb(212, 223, 65)", color: "white" }}
-                        show={showMob} onHide={()=>setShowMob(false)}
+                <div className="app_menu">
+                  <i
+                    className="bi bi-book-half"
+                    style={{ margin: "15px 0px 0px 0px" }}
+                  ></i>
+
+                  <NavDropdown
+                    title="Course"
+                    id="collasible-nav-dropdown"
+                    className="drop-menu width-2 mb-2"
+                    menuVariant="dark"
+                    style={{ margin: "5px 0px 0px 30px" }}
+                  >
+                    <NavDropdown.Item
+                      as={Link}
+                      to="/course/class_5"
+                      className="menu-item"
+                      onClick={() => setShowMob(false)}
                     >
-                        
-                        <Offcanvas.Header closeButton>
-                            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-sm`} className="me-5 ms-3 fs-1 fw-bold" style={{color:"green"}}>
-                                My School
-                            </Offcanvas.Title>
-                        </Offcanvas.Header>
+                      Class 5
+                    </NavDropdown.Item>
 
-                        <hr style={{height:"2px",color:"blue"}}/>
+                    <NavDropdown.Item
+                      as={Link}
+                      to="/course/class_6"
+                      className="menu-item"
+                      onClick={() => setShowMob(false)}
+                    >
+                      Class 6
+                    </NavDropdown.Item>
 
+                    <NavDropdown.Item
+                      as={Link}
+                      to="/course/class_7"
+                      className="menu-item"
+                      onClick={() => setShowMob(false)}
+                    >
+                      Class 7
+                    </NavDropdown.Item>
 
-                        <Offcanvas.Body className='off_body' onTouchMove={()=>setShowMob(false)}>
+                    <NavDropdown.Item
+                      as={Link}
+                      to="/course/class_8"
+                      className="menu-item"
+                      onClick={() => setShowMob(false)}
+                    >
+                      Class 8
+                    </NavDropdown.Item>
 
-                            <Nav className="justify-content-end flex-grow-1 pe-3">
-                            
-                                <Nav.Link as={NavLink} to='/' className="menu" onClick={()=>setShowMob(false)}><i className="bi bi-house-door"></i> Home</Nav.Link>
+                    <NavDropdown.Item
+                      as={Link}
+                      to="/course/class_9"
+                      className="menu-item"
+                      onClick={() => setShowMob(false)}
+                    >
+                      Class 9
+                    </NavDropdown.Item>
 
-
-                               
-                                    <Nav.Link as={NavLink}>
-                                          <i className="bi bi-book"></i>
-                                    </Nav.Link>
-
-                                <NavDropdown title="Course" id="collasible-nav-dropdown" className='menu width-2 mb-2'   menuVariant="dark" style={{margin:"-50px 0px 0px 30px"}}>
-
-                                    <NavDropdown.Item as={Link} to='/course/class_5' className='menu-item' onClick={()=>setShowMob(false)}>Class 5</NavDropdown.Item>
-                                    
-                                    <NavDropdown.Item as={Link} to='/course/class_6' className='menu-item' onClick={()=>setShowMob(false)}>Class 6</NavDropdown.Item>
-
-                                    <NavDropdown.Item as={Link} to='/course/class_7' className='menu-item' onClick={()=>setShowMob(false)}>Class 7</NavDropdown.Item>
-
-                                    <NavDropdown.Item as={Link} to='/course/class_8' className='menu-item' onClick={()=>setShowMob(false)}>Class 8</NavDropdown.Item>
-
-                                    <NavDropdown.Item as={Link} to='/course/class_9' className='menu-item' onClick={()=>setShowMob(false)}>Class 9</NavDropdown.Item>
-
-                                    <NavDropdown.Item as={Link} to='/course/class_10' className='menu-item' onClick={()=>setShowMob(false)}>Class 10</NavDropdown.Item>
-
-                                </NavDropdown>
-
-
-
-
-
-
-
-
-                                
-                                <Nav.Link as={NavLink}>
-                                     <i className="bi bi-pencil"></i>
-                                </Nav.Link>
-
-                                <NavDropdown title="Notes" id="collasible-nav-dropdown" className='menu width-2 mb-2'   menuVariant="dark" style={{margin:"-50px 0px 0px 30px"}}>
-
-                                    <NavDropdown.Item as={Link} to='/topic_notes/class_6' className='menu-item' onClick={()=>setShowMob(false)}>Class 6</NavDropdown.Item>
-                                    
-                                    <NavDropdown.Item as={Link} to='/topic_notes/class_7' className='menu-item' onClick={()=>setShowMob(false)}>Class 7</NavDropdown.Item>
-
-                                    <NavDropdown.Item as={Link} to='topic_notes/class_8' className='menu-item' onClick={()=>setShowMob(false)}>Class 8</NavDropdown.Item>
-
-                                    <NavDropdown.Item as={Link} to='topic_notes/class_9' className='menu-item' onClick={()=>setShowMob(false)}>Class 9</NavDropdown.Item>
-
-
-                                </NavDropdown>
+                    <NavDropdown.Item
+                      as={Link}
+                      to="/course/class_10"
+                      className="menu-item"
+                      onClick={() => setShowMob(false)}
+                    >
+                      Class 10
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </div>
 
 
 
+                { /*====================== Notes ======================== */   }
+
+                <div className="app_menu">
+                  <i className="bi bi-pencil-fill"></i>
+
+                  <NavDropdown
+                    title="Notes"
+                    id="collasible-nav-dropdown"
+                    className="drop-menu width-2 mb-2"
+                    menuVariant="dark"
+                    style={{ margin: "-10px 0px 0px 30px" }}
+                  >
+                    <NavDropdown.Item
+                      as={Link}
+                      to="/topic_notes/class_6"
+                      className="menu-item"
+                      onClick={() => setShowMob(false)}
+                    >
+                      Class 6
+                    </NavDropdown.Item>
+
+                    <NavDropdown.Item
+                      as={Link}
+                      to="/topic_notes/class_7"
+                      className="menu-item"
+                      onClick={() => setShowMob(false)}
+                    >
+                      Class 7
+                    </NavDropdown.Item>
+
+                    <NavDropdown.Item
+                      as={Link}
+                      to="topic_notes/class_8"
+                      className="menu-item"
+                      onClick={() => setShowMob(false)}
+                    >
+                      Class 8
+                    </NavDropdown.Item>
+
+                    <NavDropdown.Item
+                      as={Link}
+                      to="topic_notes/class_9"
+                      className="menu-item"
+                      onClick={() => setShowMob(false)}
+                    >
+                      Class 9
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </div>
+
+
+                { /*====================== Examination ======================== */   }
+
+                <div className="app_menu">
+                  <i className="bi bi-envelope-paper-fill"></i>
+
+                  <NavDropdown
+                    title="Examination Paper"
+                    id="collasible-nav-dropdown"
+                    className="drop-menu width-2 mb-2"
+                    menuVariant="dark"
+                    style={{ margin: "-10px 0px 0px 30px" }}
+                  >
+                    <NavDropdown.Item
+                      as={Link}
+                      to="/exam_paper/class_3"
+                      className="menu-item"
+                      onClick={handleExam}
+                    >
+                      Class 3
+                    </NavDropdown.Item>
+
+                    <NavDropdown.Item
+                      as={Link}
+                      to="/exam_paper/class_4"
+                      className="menu-item"
+                      onClick={handleExam}
+                    >
+                      Class 4
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </div>
 
 
 
-                                    <Nav.Link as={NavLink}>
-                                        <i className="bi bi-envelope-paper"></i>
-                                    </Nav.Link>
-                                   
-                                <NavDropdown  title="Examination Paper" id="collasible-nav-dropdown" className='menu width-2 mb-2' menuVariant="dark" style={{margin:"-50px 0px 0px 30px"}}>
+                { /*====================== Students ======================== */   }
 
-                                    <NavDropdown.Item as={Link} to="/exam_paper/class_3" className='menu-item'  onClick={handleExam}>Class 3</NavDropdown.Item>
 
-                                    <NavDropdown.Item as={Link} to="/exam_paper/class_4" className='menu-item' onClick={handleExam}>Class 4</NavDropdown.Item>
 
-                                </NavDropdown>
+                <div className="app_menu">
+                  <i className="bi bi-person-lines-fill"></i>
 
-                                
-                                <Nav.Link as={NavLink}>
-                                     <i className="bi bi-person-lines-fill"></i>
-                                </Nav.Link>
-                                
-                                
-                                                        
-                                <NavDropdown
-                                    title="Students Details"
-                                    id="collasible-nav-dropdown"
-                                    className="menu width-2"
-                                    menuVariant="dark"
-                                    style={{margin:"-50px 0px 0px 30px"}}
-                                >
-                                    <NavDropdown.Item
-                                    as={Link}
-                                    to="/add_Student_Details/class_5"
-                                    className="menu-item"
-                                    onClick={handleExam}
-                                    >
-                                    Add students Class 5
-                                    </NavDropdown.Item>
+                  <NavDropdown
+                    title="Students Details"
+                    id="collasible-nav-dropdown"
+                    className="drop-menu width-2"
+                    menuVariant="dark"
+                    style={{ margin: "-10px 0px 0px 30px" }}
+                  >
+                    <NavDropdown.Item
+                      as={Link}
+                      to="/add_Student_Details/class_5"
+                      className="menu-item"
+                      onClick={handleExam}
+                    >
+                      Add students Class 5
+                    </NavDropdown.Item>
 
-                                    <NavDropdown.Item
-                                    as={Link}
-                                    to="/add_Student_Marks/class_5"
-                                    className="menu-item"
-                                    onClick={handleExam}
-                                    >
-                                    Students Marks Class 5
-                                    </NavDropdown.Item>
+                    <NavDropdown.Item
+                      as={Link}
+                      to="/add_Student_Marks/class_5"
+                      className="menu-item"
+                      onClick={handleExam}
+                    >
+                      Students Marks Class 5
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </div>
+              </Nav>
 
-                                </NavDropdown>
-
-                            </Nav>
-
-                            {/*<Form className="navBar_form d-flex mb-3" data-bs-toggle="tooltip" data-bs-placement="top" title="Search Items">
+              {/*<Form className="navBar_form d-flex mb-3" data-bs-toggle="tooltip" data-bs-placement="top" title="Search Items">
                                 <Form.Control
                                     type="search"
                                     placeholder="Search notes,books..."
@@ -177,26 +284,36 @@ function AppBar() {
                                 <Button variant="success" className="me-5 text-white rounded-0 shadow-none" style={{ fontSize: "15px", padding: "2px 15px 2px 15px" }}>Search</Button>
                             </Form>*/}
 
-                {
-                   auth ? 
-                    <Nav>   
-                       <Nav.Link as={NavLink} to='/login' className='menu' onClick={UserLogout}>Logout</Nav.Link>
-                    </Nav> 
-                  :
-                    <Nav>
-                        <Nav.Link as={NavLink} to='/login' className="menu" onClick={() => setShowMob(false)}><i className="bi bi-person-circle"></i>  Login</Nav.Link>
-                    </Nav>  
-                }
-
-                                
-                
-                        </Offcanvas.Body>
-                    </Navbar.Offcanvas>
-
-                </Container>
-            </Navbar>
-        </>
-    );
+              {auth ? (
+                <Nav>
+                  <Nav.Link
+                    as={NavLink}
+                    to="/login"
+                    className="app_menu"
+                    onClick={UserLogout}
+                  >
+                    <i className="bi bi-box-arrow-in-right"></i>
+                    <span>Logout</span>
+                  </Nav.Link>
+                </Nav>
+              ) : (
+                <Nav>
+                  <Nav.Link
+                    as={NavLink}
+                    to="/login"
+                    className="app_menu"
+                    onClick={() => setShowMob(false)}
+                  >
+                    <i className="bi bi-person-fill-lock"></i> <span>Login</span>
+                  </Nav.Link>
+                </Nav>
+              )}
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
 
 export default AppBar;
