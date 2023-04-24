0450cab1d18ffd0a5abd394199ad98bca48aa892
kkpweb023 <kpandey023@gmail.com>
2023-04-24 21:57:13 +0530

    add class test21

--- src/Components/Paper/Class9/ClassTest1/ClassTest1.js
+++ src/Components/Paper/Class9/ClassTest1/ClassTest1.js
@@ -1,27 +1,16 @@
 import React from "react";
 import "./ClassTest1.css";
-//import Multiple from "./Questions/Multiple";
-//import TrueFalse from "./Questions/TrueFalse";
-//import Fill from "./Questions/Fill";
-//import Match from "./Questions/Match";
-//import FullForm from "./Questions/FullForm";
-//import Qusestion from "./Questions/Qusestion";
+import '../../Class8/Questions/Multiple.css'
 import jsPDF from "jspdf";
 
 const ClassTest1 = () => {
-
   const handlePaper = async () => {
-
     const pdf = new jsPDF("p", "pt", "a4");
     const data = document.querySelector("#pdf");
     pdf.html(data).then(() => {
       pdf.save("Class9.pdf");
     });
-
   };
-
-
-
 
   return (
     <>
@@ -30,9 +19,11 @@ const ClassTest1 = () => {
       </button>
 
       <div className="ClassTest" id={"pdf"}>
-        <h5 className="h5" style={{fontSize:"25px"}}>Class Test-1  Paper (2023-2024)</h5>
+        <h5 className="h5" style={{ fontSize: "25px" }}>
+          Class Test-1 Paper (2023-2024)
+        </h5>
 
-        <div className="intro" style={{fontSize:"14px"}}>
+        <div className="intro" style={{ fontSize: "14px" }}>
           <div className="marks_class_intro">
             <div className="mm" style={{ fontWeight: "bold" }}>
               M.M. 10
@@ -41,7 +32,6 @@ const ClassTest1 = () => {
               <span style={{ fontWeight: "bold" }}>Class-</span>9
             </div>
           </div>
-
 
           <div className="sub_time_intro">
             <div className="time">
@@ -60,29 +50,28 @@ const ClassTest1 = () => {
           </div>
 
           <div className="m_ques">
-        <div className="mul_question">
-          <div>a. Coordinates the components of a computer system.</div>
-          <div>
-            <span>i. ALU</span>
-            <span>ii.CU ✅</span>
-            <span>iii. Register</span>
-            <span>iv. Cache</span>
-          </div>
-        </div>
+            <div className="mul_question">
+              <div>a. Coordinates the components of a computer system.</div>
+              <div>
+                <span>i. ALU</span>
+                <span>ii.CU ✅</span>
+                <span>iii. Register</span>
+                <span>iv. Cache</span>
+              </div>
+            </div>
 
-        <div className="mul_question">
-          <div>
-            b. Which of the following is not the characteristic of computer?
+            <div className="mul_question">
+              <div>
+                b. Which of the following is not the characteristic of computer?
+              </div>
+              <div>
+                <span>i. Speed</span>
+                <span>ii. Feelings✅</span>
+                <span>iii.Obedience</span>
+                <span>iv.Storage</span>
+              </div>
+            </div>
           </div>
-          <div>
-            <span>i. Speed</span>
-            <span>ii. Feelings✅</span>
-            <span>iii.Obedience</span>
-            <span>iv.Storage</span>
-          </div>
-        </div>
-      </div>
-
 
           <div className="multi_ques">
             <div className="main_ques">
@@ -91,48 +80,76 @@ const ClassTest1 = () => {
             <div className="marks_no">2</div>
           </div>
 
-          
           <div className="true_div">
             <div className="true_question">
-              <div>a. Computer has lack of decision-making ability. <strong style={{color:"green"}}>True</strong></div>
-              <div>b.	Secondary memory is the main memory of the computer. <strong style={{color:"green"}}>False</strong></div>
+              <div>
+                a. Computer has lack of decision-making ability.{" "}
+                <strong style={{ color: "green" }}>True</strong>
+              </div>
+              <div>
+                b. Secondary memory is the main memory of the computer.{" "}
+                <strong style={{ color: "green" }}>False</strong>
+              </div>
             </div>
           </div>
 
-      
           <div className="multi_ques">
             <div className="main_ques">3. Fill in the blanks</div>
             <div className="marks_no">2</div>
           </div>
 
-        <div className="fill_div">
-          <div className="fill_question">
-            <div></div>
-          <div>a.	Most powerfull computers among digital computers are  <small style={{textDecorationLine:"underline",color:"green",fontWeight:"bold"}}>supercomputer</small>.</div>
-          <div>b.	Collecting the data and converting into information is called <small style={{textDecorationLine:"underline",color:"green",fontWeight:"bold"}}>Data Processing.</small></div>
-      </div>
-    </div>
-
-         
+          <div className="fill_div">
+            <div className="fill_question">
+              <div></div>
+              <div>
+                a. Most powerfull computers among digital computers are{" "}
+                <small
+                  style={{
+                    textDecorationLine: "underline",
+                    color: "green",
+                    fontWeight: "bold",
+                  }}
+                >
+                  supercomputer
+                </small>
+                .
+              </div>
+              <div>
+                b. Collecting the data and converting into information is called{" "}
+                <small
+                  style={{
+                    textDecorationLine: "underline",
+                    color: "green",
+                    fontWeight: "bold",
+                  }}
+                >
+                  Data Processing.
+                </small>
+              </div>
+            </div>
+          </div>
 
           <div className="multi_ques">
             <div className="main_ques">
-              6. Answer the following questions. (Any three)
+              4. Answer the following questions.
             </div>
             <div className="marks_no">4</div>
           </div>
 
           <div className="long_div">
             <div className="long_question">
-                <div>a.	What is computer and electronic device?</div>
-                <div>b.	What is parts IPO cycle define with the help  of flow chart?</div>
-                <div>c.	Write three parts of processing unit with the help of flow chart?</div>
-                <div>d.	Give two features of Micro-Computer? </div>
+              <div>a. What is computer and electronic device?</div>
+              <div>
+                b. What is parts IPO cycle define with the help of flow chart?
+              </div>
+              <div>
+                c. Write three parts of processing unit with the help of flow
+                chart?
+              </div>
+              <div>d. Give two features of Micro-Computer? </div>
             </div>
-           </div>
-
+          </div>
         </div>
-
       </div>
     </>
   );
