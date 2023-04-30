350cdcbde1cbf5490d981fe364f2147646edddf4
kkpweb023 <kpandey023@gmail.com>
2023-04-30 21:24:22 +0530

    modify c

--- src/Components/DashBoard/StdProfile.js
+++ src/Components/DashBoard/StdProfile.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 
 export const MyContext = createContext();
 
-let port = `https://cyan-encouraging-chiton.cyclic.app` || `http://localhost:4000`;
+let port = `https://unusual-elk-flannel-nightgown.cyclic.app` || `http://localhost:4000`;
 
 const StdProfile = () => {
 
