c33cf3e2e8c2c0fedf11fb9b04e6f50bedd60b64
kkpweb023 <kpandey023@gmail.com>
2024-01-15 20:47:28 +0530

    chapter 10 function modify

--- src/Components/Notes/Class9/Chapter10/Functions.js
+++ src/Components/Notes/Class9/Chapter10/Functions.js
@@ -14,7 +14,7 @@ const Functions = () => {
             <h4>Intergral parts of functions</h4>
                 <div><b>Two intergral parts of fucntions</b>: Arguments and Structure</div>
 
-              <ul>
+              <ul style={{position:"relative", right:"30px"}}>
                   <li><b>Arguments: </b> 
                       <ul>
                         <li>The values passed to the functions,using which the function carries out some task.</li>
@@ -27,31 +27,34 @@ const Functions = () => {
                       <ul>
                         <li>The structure of a function begins with the function name,followed by an opening parenthesis,the arguments for the function separated by commas, and a closing parenthesis.</li>
                         <li>Formulas begins with <b>equal sign (=)</b>.</li>
-                          <b> Syntax: </b>  = Name (argument1,argument2,...)
+                        <li><b>Syntax:</b> 
+                        <p style={{position:"relative", right:"50px"}}>=Name(argument1,argument2,...)</p></li>
                       </ul>
                   </li>           
               </ul>  
 
-              <ul>
+              <ul style={{position:"relative", right:"30px"}}>
 
                 <li><b>SUM:</b> 
                   <ul>
                     <li>This function adds all the numbers in a range of cells.</li>
-                    <b> Syntax: </b>  = SUM(number1;number2,...)
+                    <b>Syntax:</b><p style={{position:"relative", right:"40px"}}>=SUM(number1;number2,...)</p>
                   </ul>
                 </li><br/>
 
                 <li><b>AVERAGE:</b> 
                   <ul>
                     <li>This function returns the average(arithmetic mean) of the argument.</li>
-                    <b> Syntax: </b>  = AVERAGE(number1;number2,...,number30)
+                    <li><b>Syntax:</b><p style={{position:"relative", right:"40px"}}>=AVERAGE(number1;<br/>number2,...,number30) </p></li>
+                     
+                   
                   </ul>
                 </li><br/>
 
                 <li><b>COUNT:</b> 
                   <ul>
                     <li>This function counts the numbers of cells that contain numbers and numbers within the list of arguments.</li>
-                    <b> Syntax: </b>  = COUNT(value1;value2,...)
+                    <b>Syntax:</b><p style={{position:"relative", right:"40px"}}>=COUNT(value1;value2,...)</p>
                   </ul>
                 </li><br/>
 
@@ -59,35 +62,35 @@ const Functions = () => {
                   <ul>
                     <li>It refers to count all function that simply counts number of entries.</li>
                     <li>This function counts the number of values in the list of arguments.</li>
-                    <b> Syntax: </b>  = COUNT<i>A</i>(number1;number2,...,number30)
+                    <b>Syntax:</b><p style={{position:"relative", right:"40px"}}>=COUNT<i>A</i>(number1;<br/>number2,...,number30)</p>
                   </ul>
                 </li><br/>
 
                 <li><b>MAX:</b> 
                   <ul>
                     <li>This function returns the largest value in a set of values.</li>
-                    <b> Syntax: </b>  = MAX(number1;number2,...)
+                    <b>Syntax:</b><p style={{position:"relative", right:"40px"}}>=MAX(number1;number2,...)</p>
                   </ul>
                 </li><br/>
 
                 <li><b>MAX<i>A</i>:</b>
                   <ul>
                     <li>Returns the maximum of a list of argumens, including text and logical entries</li>
-                    <b> Syntax: </b>  = MAX<i>A</i>(number1;number2;...number30)
+                    <b>Syntax:</b><p style={{position:"relative", right:"40px"}}>=MAX<i>A</i>(number1;<br/>number2;...number30)</p>
                   </ul>
                 </li><br/>
 
                 <li><b>MIN:</b> 
                   <ul>
                     <li>This function returns the smallest number in a set of values.</li>
-                    <b> Syntax: </b>  = MIN(number1;number2,...)
+                    <b>Syntax:</b><p style={{position:"relative", right:"40px"}}>=MIN(number1;number2,...)</p>
                   </ul>
                 </li><br/>
 
                 <li><b>IF:</b> 
                   <ul>
                     <li>This function returns one of two values,depending on a test condition.</li>
-                    <b> Syntax: </b>  = IF(test;value1,value2)
+                    <b>Syntax:</b><p style={{position:"relative", right:"40px"}}>=IF(test;value1,value2)</p>
                   </ul>
                 </li><br/>
 
