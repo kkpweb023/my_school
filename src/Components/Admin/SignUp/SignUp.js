e5a8504d90797a24f636f971e3249362ba0c55d6
kkpweb023 <kpandey023@gmail.com>
2024-08-03 16:42:03 +0530

    render deplo

--- src/Components/Admin/SignUp/SignUp.js
+++ src/Components/Admin/SignUp/SignUp.js
@@ -5,7 +5,7 @@ import sign from '../../../Images/sign.png';
 import { Link, useNavigate } from 'react-router-dom';
 
 
-let port = `https://unusual-elk-flannel-nightgown.cyclic.app` || `http://localhost:4000`;
+let port = `https://notesdatabase-ycw0.onrender.com` || `http://localhost:4000`;
 
 
 const SignUp = () => {
